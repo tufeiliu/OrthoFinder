6aa2b83fbb8c0a250eea0f93f0e7b48231ef352b
tufeiliu <caoxen7ys@yahoo.com>
2024-04-07 16:35:33 +0100

    move PrintHelp from orthologues.py to helpinfo.py

---
+++ src/orthofinder/comparative_genomics/helpinfo.py
@@ -0,0 +1,22 @@
+from ..utils import util
+
+def PrintHelp():
+    print("Usage")    
+    print("-----")
+    print("orthologues.py orthofinder_results_directory [-t max_number_of_threads]")
+    print("orthologues.py -h")
+    print("\n")
+    
+    print("Arguments")
+    print("---------")
+    print("""orthofinder_results_directory
+    Generate gene trees for the orthogroups, generated rooted species tree and infer ortholgues.\n""")
+    
+    print(("""-t max_number_of_threads, --threads max_number_of_threads
+    The maximum number of processes to be run simultaneously. The deafult is %d but this 
+    should be increased by the user to the maximum number of cores available.\n""" % util.nThreadsDefault))
+        
+    print("""-h, --help
+   Print this help text""")
+    util.PrintCitation()       
+            
